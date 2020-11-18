c8dfdf52442981991a26bd8da192f98db2d83f27
Shoham Weiss <shohamweiss@gmail.com>
2020-11-18 15:46:30 -0600

    Add project files.

---
+++ PS5Bot/Program.cs
@@ -0,0 +1,31 @@
+﻿using ImageProcessor;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace PS5Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\shoha\source\repos\SeleniumDemo\SeleniumDemo\bin\Debug\netcoreapp3.1");
+
+            // This will open up the URL 
+            driver.Url = "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817";
+            //IWebElement email = driver.FindElement(By.Id("email"));
+            //email.SendKeys("YaronWeiss@gmail.com");
+            //IWebElement password = driver.FindElement(By.Id("pass"));
+            //password.SendKeys("koko");
+            //IWebElement login = driver.FindElement(By.Name("login"));
+            //login.Click();
+            //Thread.Sleep(2000);
+            Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+            //Save the screenshot
+            image.SaveAsFile("C:/temp/Screenshot.png");
+
+            ImageFactory Load(string imagePath);
+        }
+    }
+}
