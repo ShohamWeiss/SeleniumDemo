9c01dbd4dfaaa62304efa8a63dcc844241097dce
Shoham Weiss <shohamweiss@gmail.com>
2020-11-18 17:35:03 -0600

    CHanges of stuff

--- PS5Bot/Program.cs
+++ PS5Bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ImageProcessor;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.IO;
 using System.Threading;
@@ -15,27 +16,42 @@ namespace PS5Bot
             //IWebDriver driver = new ChromeDriver(@"C:\Users\regl9\source\repos\SeleniumDemo\SeleniumDemo\bin\Debug\netcoreapp3.1");
 
             // This will open up the URL 
-            driver.Url = "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817";
-            //IWebElement email = driver.FindElement(By.Id("email"));
-            //email.SendKeys("YaronWeiss@gmail.com");
-            //IWebElement password = driver.FindElement(By.Id("pass"));
-            //password.SendKeys("koko");
-            //IWebElement login = driver.FindElement(By.Name("login"));
-            //login.Click();
-            Thread.Sleep(1000);
-            Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
-            //Save the screenshot
+            //driver.Url = "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817";
+            driver.Url = "https://direct.playstation.com/en-us/consoles/console/";
+            //Thread.Sleep(1000);
 
             //string imagePath = @"C:\Users\regl9\source\repos\SeleniumDemo\imgs\Screenshot.png";
             string imagePath = @"C:\Temp\Screenshot.png";
-
-            image.SaveAsFile(imagePath);
-
+            //image.SaveAsFile(imagePath);
             byte[] photoBytes = File.ReadAllBytes(imagePath);
-            if (image.AsByteArray.Equals(photoBytes))
+
+            Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+            byte[] imageBytes = image.AsByteArray;
+
+            if (!areEqual(photoBytes, imageBytes))
             {
-                driver.Url = "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817";
+                // Make a sound
+                //driver.Url = "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817";
             }
+            IWebElement email = driver.FindElement(By.Id("rc-anchor-alert"));
+            IWebElement button = driver.FindElement(By.XPath("/html/body/div[1]/div/img"));
+
+            button.Click();
+        }
+        static bool areEqual(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!a[i].Equals(b[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
