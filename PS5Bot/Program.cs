9743d0a55624b29a01585c4bda6e09da1c34db2a
Regis Lato <regl96@hotmail.com>
2020-11-22 00:30:02 -0600

    updated to work with other site

--- PS5Bot/Program.cs
+++ PS5Bot/Program.cs
@@ -3,6 +3,7 @@ using NAudio.Wave;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using ServiceStack;
 using System;
 using System.Drawing;
 using System.IO;
@@ -15,27 +16,34 @@ namespace PS5Bot
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver(@"C:\Users\shoha\source\repos\SeleniumDemo\PS5Bot\bin\Debug\netcoreapp3.1");
-            //IWebDriver driver = new ChromeDriver(@"C:\Users\regl9\source\repos\SeleniumDemo\SeleniumDemo\bin\Debug\netcoreapp3.1");
+            //IWebDriver driver = new ChromeDriver(@"C:\Users\shoha\source\repos\SeleniumDemo\PS5Bot\bin\Debug\netcoreapp3.1");
+            IWebDriver driver = new ChromeDriver(@"C:\Users\regl9\source\repos\SeleniumDemo\SeleniumDemo\bin\Debug\netcoreapp3.1");
 
-            string imagePath = @"C:\Temp\Screenshot.png";
-            //string imagePath = @"C:\Users\regl9\source\repos\SeleniumDemo\imgs\Screenshot.png";
-
-            //image.SaveAsFile(imagePath);
+            //string imagePath = @"C:\Temp\Screenshot.png";
+            string imagePath = @"C:\Users\regl9\source\repos\SeleniumDemo\imgs\Screenshot.png";
+            driver.Navigate().GoToUrl(@"https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161");
+            Thread.Sleep(5000);
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, 250)");
+            Thread.Sleep(1000);
+            Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+            image.SaveAsFile(imagePath);
 
             byte[] photoBytes = File.ReadAllBytes(imagePath);
 
-            string audioFile = @"C:\Users\shoha\Music\Video Projects\Madcon - beggin lyrics.mp3";
+            string audioFile = @"C:\Windows\Media\Alarm01.wav";
             var audio = new AudioFileReader(audioFile);
             var outputDevice = new WaveOutEvent();
 
             bool run = true;
             while (run)
             {
-                driver.Navigate().GoToUrl(@"https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817");
+                //driver.Navigate().GoToUrl(@"https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817");
+                driver.Navigate().GoToUrl(@"https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161");
+                Thread.Sleep(10000);
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, 250)");
                 Thread.Sleep(1000);
-                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
-                byte[] imageBytes = image.AsByteArray;
+                Screenshot image1 = ((ITakesScreenshot)driver).GetScreenshot();
+                byte[] imageBytes = image1.AsByteArray;
 
                 if (!areEqual(photoBytes, imageBytes))
                 {
